68546af03af00da85dcde00afee5daa9494c43a8
Prithu Anan <prithua00@gmail.com>
2024-09-04 16:38:33 +0600

    Helper functions done

--- Offline 01/Main.cpp
+++ Offline 01/Main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include "NPuzzle.cpp"
 using namespace std;
 
 int main() {
-    NPuzzle npuzzle(4);
+    ifstream input_file("input.txt");
+    streambuf* cin_buf = cin.rdbuf();
+    cin.rdbuf(input_file.rdbuf());
+
+    ofstream output_file("output.txt");
+    streambuf* cout_buf = cout.rdbuf();
+    cout.rdbuf(output_file.rdbuf());
+
+    int k;
+    cin >> k;
+
+    NPuzzle npuzzle(k);
     npuzzle.initGrid();
-    npuzzle.printGrid();
+    // npuzzle.printGrid();
+    npuzzle.solve();
+
+    cin.rdbuf(cin_buf);
+    input_file.close();
+    cout.rdbuf(cout_buf);
+    output_file.close();
     return 0;
 }
--- Offline 01/NPuzzle.cpp
+++ Offline 01/NPuzzle.cpp
@@ -1,14 +1,91 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <queue>
 using namespace std;
 
-typedef vector<string> array;
-typedef vector<vector<string>> grid;
+typedef vector<string> Array;
+typedef vector<vector<string>> Grid;
 
 class NPuzzle {
     int k;
-    grid initial, goal;
+    Grid initial, goal;
+
+    struct Node {
+        Grid curr;
+        Node* prev;
+        int g;
+
+        Node(Grid& c, Node* p = nullptr, int src = 0) {            
+            curr = c;
+            prev = p;
+            g = src;
+        }
+    };
+
+    struct Compare {
+        bool operator()(Node n1, Node n2) {
+            return n1.g > n2.g;
+        }
+    };
+
+    int inversionCount() {
+        int count = 0, offset = 0;
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < k; j++) {
+                if(initial[i][j] == "*") {
+                    offset = 1;
+                    continue;
+                }
+                count += max(0, stoi(initial[i][j]) - ((i * k) + j + 1) + offset);
+            }
+        }
+        cout << count << endl;           
+        return count;
+    }
+
+    int findBlank() {
+        for(int row = 0; row < k; row++)
+            for(auto box : initial[row])
+                if(box == "*")
+                    return k - row - 1;
+    }
+
+    bool isSolvable() {
+        int inversion = inversionCount();
+        bool isInversionEven = (inversion % 2 == 0);
+        if(k % 2 != 0) return isInversionEven;
+        else {
+            int blankRow = findBlank();
+            bool isBlankRowEven = (blankRow % 2 != 0);
+            return isBlankRowEven ^ isInversionEven;
+        }
+    }
+
+    int calcHammingDistance() {
+        int count = 0;
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < k; j++) {
+                if(initial[i][j] == "*") continue;
+                if(stoi(initial[i][j]) != (i * k) + j + 1) count++;
+            }
+        }           
+        return count;
+    }
+
+    int calcManhattanDistance() {
+        int count = 0;
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < k; j++) {
+                if(initial[i][j] == "*") continue;
+                int y = 0;
+                while((y + 1) * k <= stoi(initial[i][j]))
+                    y++;
+                int x = stoi(initial[i][j]) - 1 - (y * k);
+                count += abs(i - y) + abs(j - x);
+            }
+        }           
+        return count;
+    }
 
     public:
     NPuzzle(int k) {
@@ -19,17 +96,17 @@ class NPuzzle {
                 goal[i].push_back(to_string((i * k) + j + 1));
         goal[k - 1][k - 1] = "*";
 
-        cout << "Printing Goal Grid: " << endl;
-        for(auto row : goal) {
-            for(auto box : row)
-                cout << box << " ";
-            cout << endl;
-        }
+        // cout << "Printing Goal Grid: " << endl;
+        // for(auto row : goal) {
+        //     for(auto box : row)
+        //         cout << box << " ";
+        //     cout << endl;
+        // }
     }
 
     void initGrid() {
         string input;
-        cout << "Enter Initial Grid in Row Major Order:" << endl;
+        // cout << "Enter Initial Grid in Row Major Order:" << endl;
         initial.resize(k);
         for(int i = 0; i < k; i++) {
             for(int j = 0; j < k; j++) {
@@ -37,6 +114,18 @@ class NPuzzle {
                 initial[i].push_back(input);
             }
         }
+    }
+
+    void AStarSearch() {
+        priority_queue<Node, vector<Node>, Compare> open, close;
+    }
+
+    void solve() {
+        if(!isSolvable()) {
+            cout << "Puzzle not solvable";
+            return;
+        }
+        AStarSearch();
     }
 
     void printGrid() {
